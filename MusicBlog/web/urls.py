35ed61afab9c9433be80d2b5600614a5a183bf02
Williamfox2012 <141017859+Programmingmonth@users.noreply.github.com>
2025-05-20 11:10:31 +0330

    Making buttons

--- MusicBlog/web/urls.py
+++ MusicBlog/web/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Home page
+    path('piano/', views.piano, name='piano'),
+    path('composition/', views.composition, name='composition'),
+    path('theory/', views.theory, name='theory'),
 ]
--- MusicBlog/web/views.py
+++ MusicBlog/web/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def piano(request):
+    return render(request, 'piano.html')
+
+def composition(request):
+    return render(request, 'composition.html')
+
+def theory(request):
+    return render(request, 'theory.html')
